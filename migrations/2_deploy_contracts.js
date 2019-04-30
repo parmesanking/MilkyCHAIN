e0ff8852560b510b0eb172c8c0e271ad08db07f2
Luca Agostini <parmesanking@gmail.com>
2019-04-30 18:05:18 +0200

    Truffle config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,16 @@
 // migrating the appropriate contracts
 var FarmerRole = artifacts.require("./FarmerRole.sol");
+var TransporterRole = artifacts.require("./TransporterRole.sol");
+var BottlingFarmRole = artifacts.require("./BottlingFarmRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
-var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(FarmerRole);
+  deployer.deploy(TransporterRole);
+  deployer.deploy(BottlingFarmRole);
   deployer.deploy(DistributorRole);
-  deployer.deploy(RetailerRole);
   deployer.deploy(ConsumerRole);
   deployer.deploy(SupplyChain);
 };
